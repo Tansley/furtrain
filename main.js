ec6239001a73b4ac05afdd6f43911033a86ca166
Tansley <etsue78@gmail.com>
2018-02-20 09:33:26 -0500

    redirect 7

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ function getTimeRemaining(endtime) {
   //   window.location.href = "https://furtrain.club/thank-you";
   // }, 3000));
 
-  Snipcart.subscribe('bind', 'order.completed', function() {
+  Snipcart.subscribe('order.completed', function() {
     window.location.href = "https://furtrain.club/thank-you";
   });
 
